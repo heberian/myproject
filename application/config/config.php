eeb7ffbed538c390ad4541192b062062a2c3f661
Gokul <gokul@knowtrichy.com>
2016-12-06 23:01:02 +0530

    'first'

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/velson/';
+$config['base_url'] = 'http://localhost/myproject/';
 
 /*
 |--------------------------------------------------------------------------
